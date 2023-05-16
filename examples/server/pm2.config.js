8c597c9bf2bec52e5d97f462f76ae797aeeff9b1
Alex Zelensky <alexzgit@gmail.com>
2023-05-15 20:07:58 -0700

    Simplify config file in the example server

--- examples/server/pm2.config.js
+++ examples/server/pm2.config.js
@@ -3,18 +3,15 @@
 const env = process.argv.find((el, index, arr) =>
   index > 0 && arr[index - 1] === '--env')
 
-const production = env === 'prod'
-
-const clusterOptions = production
+const clusterOptions = env === 'prod'
   ? { instances: 'max', exec_mode: 'cluster' }
-  : {}
+  : { watch: true }
 
 module.exports = {
   apps: [{
     name: 'Server',
     namespace: '@myapp/server',
     script: 'server.js',
-    watch: !production,
     env_dev: {
       NODE_ENV: 'development'
     },
