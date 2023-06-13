65f41f68cf734f95bdfa17a8752cfcce500986f9
Alex Zelensky <alexzgit@gmail.com>
2023-06-12 23:27:21 -0700

    Fix ignore flag for scripts and add --ext and --delay options

--- test/start.test.js
+++ test/start.test.js
@@ -4,7 +4,7 @@ const { custom: PM2 } = require('pm2')
 const start = require('../lib/start')
 
 const OPTIONS = {
-  file: 'test-file',
+  cmd: 'test-file',
   env: 'test-env',
   raw: true,
   ignore: []
@@ -13,7 +13,7 @@ const OPTIONS = {
 describe('start', () => {
   let connectSpy
   let startSpy
-  beforeAll(() => {
+  beforeEach(() => {
     connectSpy = jest.spyOn(PM2.prototype, 'connect')
     connectSpy.mockImplementation(cb => cb())
 
@@ -21,7 +21,7 @@ describe('start', () => {
     startSpy.mockImplementation(() => {})
   })
 
-  afterAll(() => {
+  afterEach(() => {
     jest.restoreAllMocks()
   })
 
@@ -32,11 +32,53 @@ describe('start', () => {
 
   it('calls pm2.start', () => {
     start(OPTIONS)
-    expect(startSpy.mock.calls[0][0]).toBe(OPTIONS.file)
+    expect(startSpy.mock.calls[0][0]).toBe(OPTIONS.cmd)
     expect(startSpy.mock.calls[0][1]).toEqual({
       autorestart: true,
       env: OPTIONS.env,
+      ext: '',
       ignore_watch: OPTIONS.ignore,
+      restart_delay: 2500,
+      watch: true
+    })
+  })
+
+  it('overrides default delay', () => {
+    start({ ...OPTIONS, delay: 1000 })
+    expect(startSpy.mock.calls[0][0]).toBe(OPTIONS.cmd)
+    expect(startSpy.mock.calls[0][1]).toEqual({
+      autorestart: true,
+      env: OPTIONS.env,
+      ext: '',
+      ignore_watch: OPTIONS.ignore,
+      restart_delay: 1000,
+      watch: true
+    })
+  })
+
+  it('defaults extensions when ignore specified and script passed', () => {
+    const opts = { ...OPTIONS, ignore: ['dist'] }
+    start(opts)
+    expect(startSpy.mock.calls[0][0]).toBe(opts.cmd)
+    expect(startSpy.mock.calls[0][1]).toEqual({
+      autorestart: true,
+      env: opts.env,
+      ext: 'js,jsx,json,html,xml,env,ts,tsx',
+      ignore_watch: opts.ignore.concat('node_modules'),
+      restart_delay: 2500,
+      watch: true
+    })
+  })
+
+  it('does not pass default extensions when ignore specified and config passed', () => {
+    const opts = { ...OPTIONS, ignore: ['dist'], cmd: 'config.json' }
+    start(opts)
+    expect(startSpy.mock.calls[0][0]).toBe(opts.cmd)
+    expect(startSpy.mock.calls[0][1]).toEqual({
+      autorestart: true,
+      env: opts.env,
+      ext: '',
+      ignore_watch: opts.ignore.concat('node_modules'),
       restart_delay: 2500,
       watch: true
     })
